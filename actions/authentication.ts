d11a77d6a92cc8eb8d424d6828e92f09bf988365
RuiMNFilipe <netofilipe_6@hotmail.com>
2025-04-09 19:29:35 +0100

    Add registration server action

--- actions/authentication.ts
+++ actions/authentication.ts
@@ -1,6 +1,8 @@
 "use server";
 
 import { signIn } from "@/lib/auth";
+import prisma from "@/lib/prisma";
+import { hashAndSaltPw } from "@/utils/password";
 import { AuthError } from "next-auth";
 
 export const loginAction = async (formData: FormData) => {
@@ -13,6 +15,46 @@ export const loginAction = async (formData: FormData) => {
       password,
       redirect: false,
     });
+
+    return {
+      success: true,
+      error: "",
+    };
+  } catch (e: any) {
+    if (e instanceof AuthError) {
+      return {
+        success: false,
+        error: e.type,
+      };
+    }
+
+    return {
+      success: false,
+      error: "UnknownError",
+    };
+  }
+};
+
+export const registerAction = async (formData: FormData) => {
+  const email = formData.get("email") as string;
+  const password = formData.get("password") as string;
+  const name = formData.get("name") as string;
+
+  const hashedPw = hashAndSaltPw(password);
+
+  try {
+    await prisma.user.create({
+      data: {
+        email,
+        password: hashedPw,
+        name,
+      },
+    });
+
+    return {
+      success: true,
+      error: "",
+    };
   } catch (e: any) {
     if (e instanceof AuthError) {
       return {

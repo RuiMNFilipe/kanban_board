6551d2da06ce238ddfeffa1432a4477eb5bc46fe
RuiMNFilipe <netofilipe_6@hotmail.com>
2025-04-10 19:06:20 +0100

    Add Zod validation to server actions

--- actions/authentication.ts
+++ actions/authentication.ts
@@ -2,12 +2,23 @@
 
 import { signIn, signOut } from "@/lib/auth";
 import prisma from "@/lib/prisma";
+import { registerSchema, signInSchema } from "@/lib/zod";
+import formDataToObject from "@/utils/formDataToObject";
 import { hashAndSaltPw } from "@/utils/password";
 import { AuthError } from "next-auth";
 
 export const loginAction = async (formData: FormData) => {
-  const email = formData.get("email") as string;
-  const password = formData.get("password") as string;
+  const formObject = formDataToObject(formData);
+
+  const parsed = signInSchema.safeParse(formObject);
+  if (!parsed.success) {
+    return {
+      success: false,
+      error: "ValidationError",
+    };
+  }
+
+  const { email, password } = parsed.data;
 
   try {
     await signIn("credentials", {
@@ -36,9 +47,17 @@ export const loginAction = async (formData: FormData) => {
 };
 
 export const registerAction = async (formData: FormData) => {
-  const email = formData.get("email") as string;
-  const password = formData.get("password") as string;
-  const name = formData.get("name") as string;
+  const formObject = formDataToObject(formData);
+
+  const parsed = registerSchema.safeParse(formObject);
+  if (!parsed.success) {
+    return {
+      success: false,
+      error: "ValidationError",
+    };
+  }
+
+  const { email, password, name } = parsed.data;
 
   const hashedPw = hashAndSaltPw(password);
 

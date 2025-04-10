867970a966a34234c4857518ba6ff38b507b6a0f
RuiMNFilipe <netofilipe_6@hotmail.com>
2025-04-10 15:24:08 +0100

    Add server action for retrieving specific user board

---
+++ actions/getUserBoard.ts
@@ -0,0 +1,30 @@
+"use server";
+
+import { auth } from "@/lib/auth";
+import prisma from "@/lib/prisma";
+
+export default async function getUserBoardAction(boardId: string) {
+  const session = await auth();
+
+  if (!session?.user?.email) return null;
+
+  try {
+    const board = await prisma.board.findUnique({
+      where: {
+        id: boardId,
+        user: {
+          email: session?.user?.email!,
+        },
+      },
+      select: {
+        tasks: true,
+        name: true,
+      },
+    });
+
+    return board;
+  } catch (error) {
+    console.error(error);
+    return null;
+  }
+}

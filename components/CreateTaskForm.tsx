8eefabd3e1e0aee388a38147b87a8348988cb6ad
RuiMNFilipe <netofilipe_6@hotmail.com>
2025-04-13 20:46:22 +0100

    create task creation form

---
+++ components/CreateTaskForm.tsx
@@ -0,0 +1,99 @@
+"use client";
+
+import { useForm } from "react-hook-form";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "./ui/form";
+import { z } from "zod";
+import { createTaskSchema } from "@/lib/zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { Input } from "./ui/input";
+import { Button } from "./ui/button";
+import createTaskAction from "@/actions/createTask";
+import { useState } from "react";
+
+type CreateTaskFormProps = {
+  userId: string;
+  columnId: string;
+  onClose: () => void;
+};
+
+export default function CreateTaskForm({
+  userId,
+  columnId,
+  onClose,
+}: CreateTaskFormProps) {
+  const [isLoading, setIsLoading] = useState(false);
+  const form = useForm<z.infer<typeof createTaskSchema>>({
+    resolver: zodResolver(createTaskSchema),
+    defaultValues: {
+      title: "",
+    },
+  });
+
+  async function onSubmit(values: z.infer<typeof createTaskSchema>) {
+    setIsLoading(true);
+
+    const formData = new FormData();
+    formData.append("title", values.title);
+
+    try {
+      const result = await createTaskAction(formData, userId, columnId);
+
+      if (!result.success) {
+        console.error(result.error);
+      } else {
+        onClose();
+      }
+    } catch (error) {
+      console.error(error);
+    } finally {
+      setIsLoading(false);
+    }
+  }
+
+  return (
+    <div className="bg-white p-6 rounded-lg shadow-md w-full max-w-md">
+      <h2 className="text-lg font-semibold mb-4">Create New Task</h2>
+      <Form {...form}>
+        <form onSubmit={form.handleSubmit(onSubmit)}>
+          <FormField
+            name="title"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Title</FormLabel>
+                <FormControl>
+                  <Input placeholder="Task title here..." {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <div className="flex justify-end mt-4">
+            <Button
+              type="button"
+              onClick={onClose}
+              variant="outline"
+              className="mr-2"
+              disabled={isLoading}
+            >
+              Cancel
+            </Button>
+            <Button
+              type="submit"
+              className="bg-blue-500 hover:bg-blue-700 text-white font-bold"
+              disabled={isLoading}
+            >
+              {isLoading ? "Creating..." : "Create Task"}
+            </Button>
+          </div>
+        </form>
+      </Form>
+    </div>
+  );
+}

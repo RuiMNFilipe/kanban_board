0fdfa81d9cce911ced9bf89a3ee220912a99aed0
RuiMNFilipe <netofilipe_6@hotmail.com>
2025-04-13 17:33:15 +0100

    Minor CSS fix

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -1,5 +1,9 @@
 import AuthPage from "@/components/AuthPage";
 
 export default function LoginPage() {
-  return <AuthPage type="login" />;
+  return (
+    <div className="flex items-center justify-center bg-gray-100">
+      <AuthPage type="login" />
+    </div>
+  );
 }
--- app/(auth)/register/page.tsx
+++ app/(auth)/register/page.tsx
@@ -1,5 +1,9 @@
 import AuthPage from "@/components/AuthPage";
 
 export default function RegisterPage() {
-  return <AuthPage type="register" />;
+  return (
+    <div className="flex items-center justify-center bg-gray-100">
+      <AuthPage type="register" />
+    </div>
+  );
 }

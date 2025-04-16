6afc172990102d4df8108a67a4bc7a0d2318fc70
RuiMNFilipe <netofilipe_6@hotmail.com>
2025-04-16 15:17:36 +0100

    add component

---
+++ components/TaskModal.tsx
@@ -0,0 +1,118 @@
+"use client";
+
+import { useState } from "react";
+import { Button } from "./ui/button";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "./ui/dialog";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+import { taskTitleSchema } from "@/lib/zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import createTaskAction from "@/actions/createTask";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "./ui/form";
+import { Input } from "./ui/input";
+import { Task } from "@prisma/client";
+
+type TaskModalProps = {
+  boardId: string;
+  columnId: string;
+  columnName: string;
+  onClose: () => void;
+  open: boolean;
+  setOpen: (open: boolean) => void;
+  onTaskCreate: (task: Task) => void;
+};
+
+export default function TaskModal({
+  boardId,
+  columnId,
+  columnName,
+  onClose,
+  open,
+  setOpen,
+  onTaskCreate,
+}: TaskModalProps) {
+  const form = useForm<z.infer<typeof taskTitleSchema>>({
+    resolver: zodResolver(taskTitleSchema),
+    defaultValues: {
+      title: "",
+    },
+  });
+
+  const onSubmit = async (values: z.infer<typeof taskTitleSchema>) => {
+    const formData = new FormData();
+    formData.append("title", values.title);
+
+    try {
+      const result = await createTaskAction(formData, boardId, columnId);
+
+      if (result.success) {
+        onTaskCreate(result.newTask!);
+        form.reset();
+        setOpen(false);
+        onClose();
+      }
+
+      console.log(result.error);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  return (
+    <Dialog open={open} onOpenChange={setOpen}>
+      <DialogContent>
+        <DialogHeader>
+          <DialogTitle>Create New Task</DialogTitle>
+          <DialogDescription>
+            Adding task to column: {columnName}
+          </DialogDescription>
+        </DialogHeader>
+        <Form {...form}>
+          <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-4">
+            <FormField
+              control={form.control}
+              name="title"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Task Title</FormLabel>
+                  <FormControl>
+                    <Input placeholder="Task title..." {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <DialogFooter>
+              <Button type="submit">Create</Button>
+              <Button
+                type="button"
+                variant={"secondary"}
+                onClick={() => {
+                  setOpen(false);
+                  onClose();
+                }}
+              >
+                Cancel
+              </Button>
+            </DialogFooter>
+          </form>
+        </Form>
+      </DialogContent>
+    </Dialog>
+  );
+}

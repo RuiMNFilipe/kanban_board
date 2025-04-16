198532c5bbaa0aec897b9d02cdf58f5d3772407f
RuiMNFilipe <netofilipe_6@hotmail.com>
2025-04-16 15:17:30 +0100

    add delete button on taskItem hover

--- components/TaskItem.tsx
+++ components/TaskItem.tsx
@@ -3,7 +3,10 @@
 import { useSortable } from "@dnd-kit/sortable";
 import { CSS } from "@dnd-kit/utilities";
 import { Task } from "@prisma/client";
-import { HTMLAttributes } from "react";
+import { X } from "lucide-react";
+import { HTMLAttributes, useState } from "react";
+import { Button } from "./ui/button";
+import ConfirmationModal from "./ConfirmationModal";
 
 export default function TaskItem({ task }: { task: Task }) {
   const {
@@ -21,17 +24,42 @@ export default function TaskItem({ task }: { task: Task }) {
     cursor: isDragging ? "grabbing" : "grab",
   };
 
+  const [isHovered, setIsHovered] = useState(false);
+  const [confirmationOpen, setConfirmationOpen] = useState(false);
+
+  function handleClick(e: React.MouseEvent) {
+    e.stopPropagation();
+    setConfirmationOpen(true);
+  }
+
   return (
     <div
       ref={setNodeRef}
-      style={style}
-      {...attributes}
-      {...listeners}
-      className={`p-4 mb-2 rounded shadow ${
+      className={`flex items-center justify-between p-4 mb-2 rounded shadow min-h-[68px] ${
         isDragging ? "bg-slate-200" : "bg-white"
       }`}
+      onMouseEnter={() => setIsHovered(true)}
+      onMouseLeave={() => setIsHovered(false)}
     >
-      <h3 className="font-medium">{task.title}</h3>
+      <div style={style} {...attributes} {...listeners} className="flex-1">
+        <h3 className="font-medium">{task.title}</h3>
+      </div>
+      {isHovered && (
+        <Button
+          className="bg-red-500 hover:bg-red-600 hover:cursor-pointer"
+          onClick={handleClick}
+        >
+          <X />
+        </Button>
+      )}
+      {confirmationOpen && (
+        <ConfirmationModal
+          modalTitle={`Are you sure you want to delete ${task.title}?`}
+          onClose={() => setConfirmationOpen(false)}
+          open={confirmationOpen}
+          setOpen={setConfirmationOpen}
+        />
+      )}
     </div>
   );
 }

41eb9aa79fb8f5c86b8c257188306bea7d79fcfe
RuiMNFilipe <netofilipe_6@hotmail.com>
2025-04-21 14:10:26 +0100

    Add JSDoc

--- utils/password.ts
+++ utils/password.ts
@@ -5,6 +5,17 @@ export const hashAndSaltPw = (password: string) => {
   return hashSync(password, salt);
 };
 
+/**
+ * Description
+ * @param {string} password - The user submitted password
+ * @param {string} hashedPassword - The db password hash
+ * @returns {boolean} If the hashed user password matches the hash in the db
+ *
+ * @example
+ * const password = "mypassword"
+ * const dbPassword = await db.get(password, user)
+ * verifyPassword(password, dbPassword) // true/false
+ */
 export const verifyPassword = (password: string, hashedPassword: string) => {
   return compareSync(password, hashedPassword);
 };
